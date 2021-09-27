55e34f147f398fb9a78fa0d3fd2cf74062bc7b5a
Pansystar <wh.bt3.p2@gmail.com>
2021-09-27 18:05:45 +0900

    関数構造体を追加

---
+++ Tatelier.DxLib.Optimize/Tatelier.DxLib.Optimize/FunctionStruct.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tatelier.DxLib.Optimize
+{
+    class Parameter
+    {
+        public string Type;
+
+        public string Name;
+
+        public string defaultValue;
+    }
+
+    class Function
+    {
+        public string ReturnType;
+
+        public string Name;
+
+        public bool IsUnsafe = false;
+
+        public List<Parameter> ParameterList = new List<Parameter>();
+
+        public string GetParameterString(bool ignoreType = false, bool ignoreDefaultValue = false)
+        {
+            string result = "";
+
+            foreach(var item in ParameterList)
+            {
+                result += $"{(ignoreType ? "" : $"{item.Type} ")}{item.Name}{(ignoreDefaultValue ? "" : (item.defaultValue != null ? $" = {item.defaultValue}" : ""))}, ";
+            }
+
+            if (result.Length > 0)
+            {
+                result = result.Substring(0, result.Length - 2);
+            }
+            return result;
+        }
+
+        public string GetString(string header, string functionNameHeader = "", bool ignoreDefaultValue = false)
+        {
+            return $"{header}{ReturnType} {functionNameHeader}{Name}({GetParameterString(ignoreDefaultValue: ignoreDefaultValue)})";
+        }
+
+        public void Clear()
+        {
+            ReturnType = null;
+            Name = null;
+            IsUnsafe = false;
+            ParameterList.Clear();
+        }
+    }
+}
